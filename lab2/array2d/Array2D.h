6d8fabd8157e882dd7c04e158b4fdc92a8282f1d
camisalo <konrad.gebczynski@gmail.com>
2017-03-11 12:41:18 +0100

    próba

---
+++ lab2/array2d/Array2D.h
@@ -0,0 +1,12 @@
+//
+// Created by konrad on 11.03.17.
+//
+
+#ifndef JIMP_EXERCISES_ARRAY2D_H
+#define JIMP_EXERCISES_ARRAY2D_H
+
+int **Array2D(int n_rows, int n_columns);
+void DeleteArray2D(int **array, int n_rows, int n_columns);
+
+
+#endif //JIMP_EXERCISES_ARRAY2D_H
