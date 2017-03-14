9e51c09c1b702721feff634d93dcc09933bbe564
camisalo <konrad.gebczynski@gmail.com>
2017-03-14 18:23:26 +0100

    ready

--- lab2/array2d/Array2D.cpp
+++ lab2/array2d/Array2D.cpp
@@ -4,11 +4,29 @@
 
 #include "Array2D.h"
 
+#include <iostream>
+
+using namespace std;
 
 int **Array2D(int n_rows, int n_columns){
-
+    if (n_rows==0 || n_columns==0){
+        std::cout << "bad_array_new_length" << std::endl;
+        return nullptr;
+    }
+    std::cout << endl;
+    int ** tab = new int * [n_rows];
+    for (int i=0; i<n_rows; ++i){
+        tab[i] = new int [n_columns];
+        for (int j=0; j < n_columns; ++j){
+            tab[i][j] = j + i * n_columns + 1;
+        }
+    }
+    return tab;
 }
 
 void DeleteArray2D(int **array, int n_rows, int n_columns){
-
+    for (int i=0; n_rows > i; ++i){
+        delete [] array[i];
+    }
+    delete [] array;
 }
