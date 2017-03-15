26a073259eb264b343ae5d3ee7a008e44ca0e826
camisalo <konrad.gebczynski@gmail.com>
2017-03-15 15:49:45 +0100

    TinyUrl metody

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -2,4 +2,17 @@
 // Created by konrad on 15.03.17.
 //
 
+#include <utility>
+#include <string>
+#include <array>
+#include <memory>
+
 #include "TinyUrl.h"
+
+std::unique_ptr<TinyUrlCodec> Init();
+
+void NextHash(std::arrray<char, 6> *state);
+
+std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
