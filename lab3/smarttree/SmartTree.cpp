3f3086edac8e23ea540b061d96b341955f60bb73
camisalo <konrad.gebczynski@gmail.com>
2017-03-15 16:12:50 +0100

    smarttree source files

--- lab3/smarttree/SmartTree.cpp
+++ lab3/smarttree/SmartTree.cpp
@@ -1,5 +1,33 @@
 //
 // Created by konrad on 15.03.17.
 //
+#include <ostream>
+#include <string>
+#include <memory>
 
 #include "SmartTree.h"
+
+
+std::unique_ptr<SmartTree> CreateLeaf(int value) {
+
+}
+
+std::unique_ptr<SmartTree> InsertLeftChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> left_subtree) {
+
+}
+
+std::unique_ptr<SmartTree> InsertRightChild(std::unique_ptr<SmartTree> tree, std::unique_ptr<SmartTree> right_subtree) {
+
+}
+
+void PrintTreeInOrder(const std::unique_ptr<SmartTree> &unique_ptr, std::ostream *out) {
+
+}
+
+std::string DumpTree(const std::unique_ptr<SmartTree> &tree) {
+
+}
+
+std::unique_ptr<SmartTree> RestoreTree(const std::string &tree) {
+
+}
--- lab3/smarttree/main.cpp
+++ lab3/smarttree/main.cpp
@@ -2,3 +2,7 @@
 // Created by konrad on 15.03.17.
 //
 
+int main() {
+
+    return 0;
+}
