2975314f8f8e854909bd94f259a12058c93abee8
camisalo <konrad.gebczynski@gmail.com>
2017-03-15 15:45:44 +0100

    TinyUrl pliki

---
+++ lab3/tinyurl/TinyUrl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by konrad on 15.03.17.
+//
+
+#include "TinyUrl.h"
---
+++ lab3/tinyurl/TinyUrl.h
@@ -0,0 +1,14 @@
+//
+// Created by konrad on 15.03.17.
+//
+
+#ifndef JIMP_EXERCISES_TINYURL_H
+#define JIMP_EXERCISES_TINYURL_H
+
+
+class TinyUrl {
+
+};
+
+
+#endif //JIMP_EXERCISES_TINYURL_H
