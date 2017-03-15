0b1de67e19b203460c6f7ec9e3651db9c0dc56fe
camisalo <konrad.gebczynski@gmail.com>
2017-03-15 12:08:43 +0100

    rozwiazanie simpleforwardlist

---
+++ lab2/simpleforwardlist/SimpleForwardList.h
@@ -0,0 +1,19 @@
+//
+// Created by konrad on 14.03.17.
+//
+
+#ifndef JIMP_EXERCISES_SIMPLEFORWARDLIST_H
+#define JIMP_EXERCISES_SIMPLEFORWARDLIST_H
+
+struct ForwardList //
+{
+    int number;
+    struct ForwardList *next;
+};
+
+
+ForwardList *CreateNode(int value);
+
+void DestroyList(ForwardList *list);
+
+#endif //JIMP_EXERCISES_SIMPLEFORWARDLIST_H
