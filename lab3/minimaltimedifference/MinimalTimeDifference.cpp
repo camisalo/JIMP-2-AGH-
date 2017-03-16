9ebc86c8908991defa8166e08aaaf6eef3fc96bf
camisalo <konrad.gebczynski@gmail.com>
2017-03-16 09:48:39 +0100

    czas poczatek

---
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -0,0 +1,22 @@
+//
+// Created by konrad on 16.03.17.
+//
+
+
+#include <vector>
+#include <sstream>
+#include <regex>
+#include <cmath>
+
+#include "MinimalTimeDifference.h"
+
+using namespace std;
+
+unsigned int ToMinutes(std::string time_HH_MM) {
+    cout << time_HH_MM << endl;
+
+}
+
+unsigned int MinimalTimeDifference(std::vector<std::string> times) {
+
+}
---
+++ lab3/minimaltimedifference/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by konrad on 16.03.17.
+//
+
+int main(void) {
+
+
+    return 0;
+}
