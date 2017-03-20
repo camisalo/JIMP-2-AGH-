a035066fdde7a1a2f001b0ff20ead1dc861e9922
konrad <konrad.gebczynski@gmail.com>
2017-03-20 21:36:35 +0100

    SmatrTree ready

--- lab3/smarttree/SmartTree.cpp
+++ lab3/smarttree/SmartTree.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <memory>
 #include <iostream>
+#include <regex>
 
 #include "SmartTree.h"
 
@@ -64,14 +65,64 @@ std::string datastructures::Print(const std::unique_ptr<SmartTree> &tree) {
 }
 
 std::unique_ptr<SmartTree> datastructures::RestoreTree(const std::string &tree) {
-    int i = 0, j;
-    std::string number;
-    while (tree[i] = '[') {
+    std::unique_ptr<SmartTree> p;
+    int i=0;
+    int left=0;
+    std::string next_tree;
+    std::string value;
+    bool ok = false;
+    while (tree[i] == '[' || tree[i] == ' '){
         i++;
-    }
+        while (tree[i] != '[' && tree[i] != ']'){
+            value += tree[i];
+            i++;
+        }
 
-    while (tree[i] != '[' || tree[i] != ']') {
+        if (value == "none"){
+            return nullptr;
+        }
+        else{
+            p = CreateLeaf(std::stoi(value));
+
+            while (!ok){
+                if (tree[i] == '[')
+                    left++;
+                else if (tree[i] == ']')
+                    left--;
+
+                next_tree += tree[i];
+
+                if (left == 0)
+                    ok = true;
+                else
+                    i++;
+            }
+            i++;
+            i++;
+            cout << "left = " << next_tree << "          ";
+            p->left = RestoreTree(next_tree);
+
+            next_tree = "";
+            ok = false;
+
+            while (!ok){
+                if (tree[i] == '[')
+                    left++;
+                else if (tree[i] == ']')
+                    left--;
+
+                next_tree += tree[i];
+
+                if (left == 0)
+                    ok = true;
+                else
+                    i++;
+            }
+            cout << "right = " << next_tree << endl;
+            p->right = RestoreTree(next_tree);
+        }
 
     }
+    return p;
 }
 
--- lab3/smarttree/main.cpp
+++ lab3/smarttree/main.cpp
@@ -1,8 +1,14 @@
 //
 // Created by konrad on 15.03.17.
 //
+#include <iostream>
+
+#include "SmartTree.h"
 
 int main() {
 
+    auto tree = datastructures::RestoreTree("[99 [100 [1234 [none] [none]] [4321 [none] [none]]] "
+                                    "[88 [897 [none] [none]] [761 [none] [none]]]]");
+    std::cout << datastructures::DumpTree(tree);
     return 0;
 }
