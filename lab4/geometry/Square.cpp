722ed618ee885b298369fbc4b7598d7e2b4c0d65
konrad <konrad.gebczynski@gmail.com>
2017-03-21 21:35:06 +0100

    Synchronizacja

--- lab4/geometry/Square.cpp
+++ lab4/geometry/Square.cpp
@@ -2,4 +2,94 @@
 // Created by konrad on 21.03.17.
 //
 
+
+
+#include <cmath>
+#include <ostream>
+
 #include "Square.h"
+
+
+using ::std::ostream;
+using ::std::endl;
+using ::std::pow;
+using ::std::sqrt;
+using ::std::cout;
+
+
+geometry::Point::Point() : x_(0), y_(0) {}
+
+geometry::Point::Point(double x, double y) {
+    x_ = x;
+    y_ = y;
+}
+
+geometry::Point::~Point() {}
+
+double geometry::Point::Distance(const Point &other) const {
+    return sqrt(pow(GetX() - other.GetX(), 2) + pow(GetY() - other.GetY(), 2));
+}
+
+void geometry::Point::ToString(ostream *out) const {
+    (*out) << "(" << GetX() << ";" << GetY() << ")";
+}
+
+double geometry::Point::GetX() const {
+    return x_;
+}
+
+double geometry::Point::GetY() const {
+    return y_;
+}
+
+void geometry::Point::SetX(double x) {
+    x_ = x;
+}
+
+void geometry::Point::SetY(double y) {
+    y_ = y;
+}
+
+
+geometry::Square::Square() {
+
+}
+
+geometry::Square::Square(Point p1, Point p2, Point p3, Point p4) {
+    this->p1 = p1;
+    this->p2 = p2;
+    this->p3 = p3;
+    this->p4 = p4;
+}
+
+geometry::Square::~Square() {}
+
+double geometry::Square::Circumference() const {
+    double Circumference = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (p[0].Distance(p[1]) == p[i].Distance(p[i + 1]))
+            Circumference += p[i].Distance(p[i + 1]);
+        else {
+            cout << "SIEMA" << endl;
+            Circumference = 0;
+            return Circumference;
+        }
+    }
+    cout << endl << Circumference << endl;
+    return Circumference;
+
+}
+
+double geometry::Square::Area() const {
+    double Area = 0;
+    for (int i = 0; i < 4; ++i) {
+        if (p[0].Distance(p[1]) == p[i].Distance(p[i + 1])) {}
+        else {
+            cout << "SIEMA" << endl;
+            return 0;
+        }
+    }
+    Area = p[0].Distance(p[1]) * p[1].Distance(p[2]);
+    cout << endl << Area << endl;
+    return Area;
+}
--- lab4/geometry/Square.h
+++ lab4/geometry/Square.h
@@ -5,10 +5,64 @@
 #ifndef JIMP_EXERCISES_SQUARE_H
 #define JIMP_EXERCISES_SQUARE_H
 
+#include <iostream>
 
-class Square {
+namespace geometry {
 
-};
+    class Point {
+    public:
+        //Konstruktor bezparametrowy
+        Point();
+
+        //Konstruktor parametrowy
+        Point(double x, double y);
+
+        //Destruktor wykonywany przed zwolnieniem pamięci
+        ~Point();
+
+        //Metody nie modyfikujące stanu obiektu (const na końcu metody)
+        //nie mogą zmodyfikować tego obiektu.
+        void ToString(std::ostream *out) const;
+
+        double Distance(const Point &other) const;
 
 
+        //metody akcesorów są publiczne i tylko w przy ich pomocy
+        //można się dostać z zewnątrz do pól klasy
+        double GetX() const;
+
+        double GetY() const;
+
+        //metody seterów pozwalające zmienić stan obiektu
+        //po jego zainicjalizowaniu
+        void SetX(double x);
+
+        void SetY(double y);
+
+    private:
+        //w przeciwienstwie do pythona C++ wymaga jawnej deklaracji składowych pól klasy:
+        double x_, y_;
+    };
+
+
+    class Square {
+    public:
+        Square();
+
+        Square(Point p1, Point p2, Point p3, Point p4);
+
+        ~Square();
+
+        double Circumference() const;
+
+        double Area() const;
+
+    private:
+        Point p1;
+        Point p2;
+        Point p3;
+        Point p4;
+        Point p[5] = {p1, p2, p3, p4, p1};
+    };
+}
 #endif //JIMP_EXERCISES_SQUARE_H
