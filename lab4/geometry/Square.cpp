d1f6ee32644c6e04a7d040a72bb28385ba9792eb
camisalo <konrad.gebczynski@gmail.com>
2017-03-22 19:49:55 +0100

    geometry

--- lab4/geometry/Square.cpp
+++ lab4/geometry/Square.cpp
@@ -65,31 +65,54 @@ geometry::Square::Square(Point p1, Point p2, Point p3, Point p4) {
 geometry::Square::~Square() {}
 
 double geometry::Square::Circumference() const {
-    double Circumference = 0;
-    for (int i = 0; i < 4; ++i) {
-        if (p[0].Distance(p[1]) == p[i].Distance(p[i + 1]))
-            Circumference += p[i].Distance(p[i + 1]);
-        else {
-            cout << "SIEMA" << endl;
-            Circumference = 0;
-            return Circumference;
+    double circumference = 0, length;
+    int k = 0;
+    length = p1.Distance(p2);
+    for (int j = 0; j < 3; ++j) {
+        for (int i = j + 1; i < 4; ++i) {
+            if (length < p[j].Distance(p[i])) {
+                length = p[j].Distance(p[i]);
+                k = 0;
+                circumference = 0;
+            } else if (length == p[j].Distance(p[i])) {
+                k++;
+                circumference += p[j].Distance(p[i]);
+            } else {
+                cout << k << "za dlugi odcinek" << endl;
+            }
         }
     }
-    cout << endl << Circumference << endl;
-    return Circumference;
-
+    if (k == 4) {
+        cout << endl << circumference << endl;
+    } else {
+        circumference = 0;
+    }
+    cout << circumference << endl;
+    return circumference;
 }
 
 double geometry::Square::Area() const {
-    double Area = 0;
-    for (int i = 0; i < 4; ++i) {
-        if (p[0].Distance(p[1]) == p[i].Distance(p[i + 1])) {}
-        else {
-            cout << "SIEMA" << endl;
-            return 0;
+    double Area = 0, length;
+    int k = 0;
+    length = p[0].Distance(p[1]);
+    for (int j = 0; j < 3; ++j) {
+        for (int i = j + 1; i < 4; ++i) {
+            if (length < p[j].Distance(p[i])) {
+                length = p[j].Distance(p[i]);
+                k = 0;
+            } else if (length == p[j].Distance(p[i])) {
+                k++;
+            } else {
+                cout << k << "za dlugi odcinek" << endl;
+            }
         }
     }
-    Area = p[0].Distance(p[1]) * p[1].Distance(p[2]);
-    cout << endl << Area << endl;
+    if (k == 4) {
+        Area = p[0].Distance(p[1]) * p[1].Distance(p[2]);
+        cout << endl << Area << endl;
+    } else {
+        Area = 0;
+    }
+    cout << Area << endl;
     return Area;
 }
