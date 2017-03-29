f768d37ef49b4af8e7556771d24f6b6829468cc1
konrad <konrad.gebczynski@gmail.com>
2017-03-29 19:32:06 +0200

    SimpleTemplateEngine ---> (all test passed also Injection test)

--- lab4/netstemplateengine/main.cpp
+++ lab4/netstemplateengine/main.cpp
@@ -2,7 +2,25 @@
 // Created by konrad on 28.03.17.
 //
 
+#include "SimpleTemplateEngine.h"
+
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <iostream>
+#include <memory>
+
+using std::cout;
+using std::endl;
+
 int main(void) {
+
+    const auto p = std::make_unique<nets::View>("siema jestem sobie {{ktos}} i nazywkam sie Lalalaa.");
+
+
+    std::unordered_map<string, string> por = {{"ktos", "Konrad"}};
+
+    cout << p->Render(por) << endl;
 
     return 1;
 }
