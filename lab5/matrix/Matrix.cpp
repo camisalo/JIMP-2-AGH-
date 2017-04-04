116c06988e1e8338fe2d695b58f9dbe8bbc5f9cd
camisalo <konrad.gebczynski@gmail.com>
2017-04-04 12:41:43 +0200

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises into lab5
    # Conflicts:
    #	lab5/CMakeLists.txt

--- lab5/matrix/Matrix.cpp
+++ lab5/matrix/Matrix.cpp
@@ -4,9 +4,49 @@
 
 #include "Matrix.h"
 
+namespace algebra {
 
-Matrix::Matrix() {
+    Matrix::Matrix() {
+        rows = 0;
+        columns = 0;
+    }
+
+    Matrix::Matrix(const char *text) {
+        for (int i = 0; text[i] != '\0'; ++i) {
+
+        }
+    }
+
+    Matrix::Matrix(const Matrix &tex) {
+
+    }
+
+    Matrix::~Matrix() {
+
+    }
+
+
+    Matrix::Add(const Matrix &p) const {
+
+    }
+
+    Matrix::Sub(const Matrix &p) const {
+
+    }
+
+    Matrix::Mul(const Matrix &p) const {
+
+    }
+
+
+    std::string Matrix::Print() const {
+
+
+    }
+
+    size_t Matrix::Size() const {
+
+    }
+
 
 }
-
-Matrix::Matrix() {}
