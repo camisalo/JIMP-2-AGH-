9000693c45321a93ba9d147e20125e3d9df65310
camisalo <konrad.gebczynski@gmail.com>
2017-04-18 00:01:43 +0200

    WordCounter --> work in progress

--- lab6/wordcounter/WordCounter.cpp
+++ lab6/wordcounter/WordCounter.cpp
@@ -6,30 +6,125 @@
 
 namespace datastructures {
 
+    ////////////////////////////////// --------  WORDCOUNTER ---- WORDCOUNTER ---- WORDCOUNTER --- /////////////////////////////
+
+
     WordCounter::WordCounter() {}
 
     WordCounter::WordCounter(std::initializer_list<Word> init) {
+        bool ok;
         for (auto v:init) {
-
+            ok = false;
+            for (auto k:contener) {
+                if (k.first == v) {
+                    ++k.second;
+                    ok = true;
+                }
+            }
+            if (!ok) {
+                std::pair<Word, Counts> p;
+                p.first = v;
+                p.second = Counts();
+                contener.push_back(p);
+            }
         }
     }
 
 
-    int WordCounter::DistinctWords() const {
-
+    size_t WordCounter::DistinctWords() const {
+        return contener.size();
     }
 
-    int WordCounter::TotalWords() const {
-
+    size_t WordCounter::TotalWords() const {
+        int sum = 0;
+        for (auto v:contener) {
+            sum += v.second.GetCount();
+        }
+        return (size_t) sum;
     }
 
+    std::set<Word> WordCounter::Words() {
+        std::sort(contener.begin(), contener.end());
+        std::set<Word> SortedWords;
+        for (auto v:contener) {
+            SortedWords.emplace(v.first);
+        }
+        return SortedWords;
+    }
+
+    Counts WordCounter::operator[](std::string word) {
+        for (auto v:contener) {
+            if (word == v.first.GetWord());
+            return v.second;
+        }
+        return Counts(0);
+    }
+
+    ////////////////////////////////// --------  WORD ---- WORD ---- WORD --- /////////////////////////////
+
+    Word::Word() {}
 
     Word::Word(std::string init) : word{init} {}
 
-
-    Counts::Counts() {
-
+    std::string Word::GetWord() const {
+        return word;
     }
+
+    void Word::SetWord(std::string set) {
+        word = set;
+    }
+
+    std::ostream &operator<<(std::ostream &output, Word &in) {
+        std::string out = in.GetWord();
+        output << out;
+        return output;
+    }
+
+    bool operator==(const Word &L, const Word &R) {
+        return (L.GetWord() == R.GetWord());
+    }
+
+    bool operator>(const Word &L, const Word &R) {
+        return (L.GetWord() > R.GetWord());
+    }
+
+    bool operator<(const Word &L, const Word &R) {
+        return (L.GetWord() > R.GetWord());
+    }
+
+
+    ////////////////////////////////// --------  COUNTS ---- COUNTS ---- COUNTS --- /////////////////////////////
+
+
+    Counts::Counts() : count{1} {}
+
+    Counts::Counts(int init) : count{init} {}
+
+    int Counts::GetCount() const {
+        return count;
+    }
+
+    void Counts::SetCount(int set) {
+        count = set;
+    }
+
+    Counts &Counts::operator++() {
+        ++count;
+    }
+
+
+    bool operator==(const Counts &L, const Counts &R) {
+        return (L.GetCount() == R.GetCount());
+    }
+
+    bool operator>(const Counts &L, const Counts &R) {
+        return (L.GetCount() > R.GetCount());
+    }
+
+    bool operator<(const Counts &L, const Counts &R) {
+        return (L.GetCount() > R.GetCount());
+    }
+
 
 
 }
--- lab6/wordcounter/WordCounter.h
+++ lab6/wordcounter/WordCounter.h
@@ -8,37 +8,73 @@
 #include <initializer_list>
 #include <string>
 #include <vector>
-
+#include <istream>
+#include <set>
+#include <algorithm>
 
 namespace datastructures {
+    ////////////////////////////////// --------  WORD ---- WORD ---- WORD --- /////////////////////////////
     class Word {
     public:
+        Word();
+
         Word(std::string init);
 
+        std::string GetWord() const;
+
+        void SetWord(std::string set);
 
     private:
         std::string word;
     };
 
+    /////////// PRZEŁADOWANE OPERATORY //////////////////////////
+    std::ostream &operator<<(std::ostream &, Word &);
+
+    bool operator==(const Word &L, const Word &R);
+
+    bool operator>(const Word &L, const Word &R);
+
+    bool operator<(const Word &L, const Word &R);
+
+    ////////////////////////////////// --------  COUNTS ---- COUNTS ---- COUNTS --- /////////////////////////////
     class Counts {
     public:
         Counts();
+
+        Counts(int init);
+
+        int GetCount() const;
+
+        void SetCount(int set);
+
+        Counts &operator++();
 
     private:
         int count;
     };
 
+    /////////// PRZEŁADOWANE OPERATORY //////////////////////////
+    bool operator==(const Counts &L, const Counts &R);
 
+    bool operator>(const Counts &L, const Counts &R);
+
+    bool operator<(const Counts &L, const Counts &R);
+
+    ////////////////////////////////// --------  WORDCOUNTER ---- WORDCOUNTER ---- WORDCOUNTER --- /////////////////////////////
     class WordCounter {
     public:
         WordCounter();
 
         WordCounter(std::initializer_list<Word> init);
 
+        size_t DistinctWords() const;
 
-        int DistinctWords() const;
+        size_t TotalWords() const;
 
-        int TotalWords() const;
+        std::set<Word> Words();
+
+        Counts operator[](std::string word);
 
     private:
         std::vector<std::pair<Word, Counts>> contener;
