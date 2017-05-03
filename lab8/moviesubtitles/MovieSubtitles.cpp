db6b2194674b348c6008f5456a3acc6ca70e8eb7
camisalo <konrad.gebczynski@gmail.com>
2017-05-03 13:59:32 +0200

    MicroDvd ok SubRip prawie ok

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -22,7 +22,8 @@ namespace moviesubs {
     MicroDvdSubtitles::~MicroDvdSubtitles() {}
 
 
-    void MicroDvdSubtitles::ShiftAllSubtitlesBy(int timetomove, int framerate, std::stringstream *in,
+    void MicroDvdSubtitles::ShiftAllSubtitlesBy(int timetomove, int framerate,
+                                                std::stringstream *in,
                                                 std::stringstream *out) const {
         int frametomove = ((timetomove * framerate) / 1000);
         char line[300];
@@ -44,7 +45,8 @@ namespace moviesubs {
                     stop += line[i];
                     i++;
                 }
-                *out << (std::stoi(start) + frametomove) << "}{" << (std::stoi(stop) + frametomove) << "}";
+                *out << (std::stoi(start) + frametomove) << "}{"
+                     << (std::stoi(stop) + frametomove) << "}";
                 i++;
                 while (line[i] != '\0') {
                     *out << line[i];
@@ -61,10 +63,99 @@ namespace moviesubs {
     SubRipSubtitles::~SubRipSubtitles() {}
 
 
-    void SubRipSubtitles::ShiftAllSubtitlesBy(int timetomove, int framerate, std::stringstream *in,
+    void SubRipSubtitles::ShiftAllSubtitlesBy(int timetomove, int framerate,
+                                              std::stringstream *in,
                                               std::stringstream *out) const {
+        int hr, min, sec, msec, TIME;
+        int lineNuber, i;
+        char line[300];
+        std::string temp;
 
+        while (in->getline(line, 300)) {
+            i = 0;
+            while (line[i] != '\0') {
+                *out << line[i];
+                i++;
+            }
+            *out << '\n';
+            i = 0;
+            in->getline(line, 300);
+            for (int j = 0; j < 2; ++j) {
+                TIME = timetomove;
+                temp = "";
+                while (line[i] != ':') {
+                    temp += line[i];
+                    i++;
+                }
+                TIME += std::stoi(temp) * 3600000;
+                temp = "";
+                i++;
+
+                while (line[i] != ':') {
+                    temp += line[i];
+                    i++;
+                }
+                TIME += std::stoi(temp) * 60000;
+                temp = "";
+                i++;
+
+                while (line[i] != ',') {
+                    temp += line[i];
+                    i++;
+                }
+                TIME += std::stoi(temp) * 1000;
+                temp = "";
+                i++;
+
+                while (line[i] != ' ') {
+                    temp += line[i];
+                    i++;
+                }
+                TIME += std::stoi(temp);
+
+                hr = TIME / 3600000;
+                min = (TIME - (hr * 3600000)) / 60000;
+                sec = (TIME - (hr * 3600000) - (min * 60000)) / 1000;
+                msec = TIME % 1000;
+
+                if (hr < 10)
+                    *out << "0" << hr << ":";
+                else
+                    *out << hr << ":";
+
+                if (min < 10)
+                    *out << "0" << min << ":";
+                else
+                    *out << min << ":";
+
+                if (sec < 10)
+                    *out << "0" << sec << ",";
+                else
+                    *out << sec << ",";
+
+                if (msec < 100)
+                    *out << "0" << msec;
+                else if (msec < 10)
+                    *out << "00" << msec;
+                else
+                    *out << msec;
+
+                if (j == 0)
+                    *out << " --> ";
+
+                i += 5;
+            }
+            *out << "\n";
+
+            while (in->getline(line, 300) && line[0] != '\0') {
+                i = 0;
+                while (line[i] != '\0') {
+                    *out << line[i];
+                    i++;
+                }
+                *out << '\n';
+            }
+            *out << '\n';
+        }
     }
-
-
 }
